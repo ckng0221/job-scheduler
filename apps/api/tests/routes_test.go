530e67bcc5466e7bb846ff4e26e1156f380de140
CK Ng <ngchoonkhon@gmail.com>
2024-03-20 19:09:07 +0800

    test: add unit test for routes and time

---
+++ apps/api/tests/routes_test.go
@@ -0,0 +1,20 @@
+package tests
+
+import (
+	"job-scheduler/api/routes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRoutes(t *testing.T) {
+	router := routes.SetupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
---
+++ apps/api/tests/utils_test.go
@@ -0,0 +1,30 @@
+package tests
+
+import (
+	"fmt"
+	"job-scheduler/api/utils"
+	"testing"
+	"time"
+)
+
+func TestGetUnixMinuteRange(t *testing.T) {
+	testCases := []struct {
+		input    int64
+		minStart int64
+		minEnd   int64
+	}{
+
+		{1710900325, 1710900300, 1710900360},
+		{1704038430, 1704038400, 1704038460},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(fmt.Sprint(testCase.input), func(t *testing.T) {
+			currentTime := time.Unix(testCase.input, 0)
+			currentMinute, nextMinute := utils.GetUnixMinuteRange(currentTime)
+			if currentMinute.Unix() != testCase.minStart || nextMinute.Unix() != testCase.minEnd {
+				t.Errorf("Error. Expected %v, %v. Got %v, %v", testCase.minStart, testCase.minEnd, currentMinute.Unix(), nextMinute.Unix())
+			}
+		})
+	}
+}
